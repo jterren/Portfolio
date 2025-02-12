51412e6decc804d8ac09d71b3907c11912983f2a
jterren <jterren@gmail.com>
2025-02-12 13:34:06 -0500

    Vercel setup and Unity WebGL iframe (in progress)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./global.css";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="p-3 flex justify-center items-center min-h-screen bg-black">
+        <iframe
+          src="http://127.0.0.1:3000/about"
+          width="100%"
+          height="100%"
+          className="border-none min-h-screen w-full"
+          style={{ height: "100vh" }}
+        />
+      </div>
+    </>
+  );
 }
