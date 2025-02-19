8153c824e98da8e7666632a7b2667112f664dc86
jterren <jterren@gmail.com>
2025-02-19 13:13:51 -0500

    Create cache for PDFData, reduce server compute

--- src/pages/api/getPdf.ts
+++ src/pages/api/getPdf.ts
@@ -1,14 +1,25 @@
 import path from "path";
 import { getPdfJson } from "../../helpers";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { unstable_cache } from "next/cache";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
+    const pdfData = unstable_cache(
+      async () => {
+        return await getPdfJson(path.resolve("./public", "Terren_Resume.pdf"));
+      },
+      [],
+      {
+        revalidate: 3600,
+        tags: ["Terren_Resume.pdf"],
+      }
+    );
     res.status(200).json({
-      data: await getPdfJson(path.resolve("./public", "Terren_Resume.pdf")),
+      data: pdfData,
     });
   } catch (err) {
     console.log(err);
