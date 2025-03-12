1fd46d335ff89278881a62e03cc3f4470cae6565
jterren <jterren@gmail.com>
2025-03-12 11:19:58 -0400

    Center loading element

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <Image
-      src={"/slime_bounce.gif"}
-      alt="Bouncing Slime by Jake Terren"
-      width={64}
-      height={64}
-      unoptimized
-    />
+    <div className="d-flex justify-content-center">
+      <Image
+        src="/slime_bounce.gif"
+        alt="Bouncing Slime by Jake Terren"
+        width={64}
+        height={64}
+        unoptimized
+      />
+    </div>
   );
 }
