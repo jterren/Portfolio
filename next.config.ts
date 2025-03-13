115c156d192ad133d8b3f77b4b404359127115f0
jterren <jterren@gmail.com>
2025-03-12 20:38:13 -0400

    Unity Game integration QA ONLY

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    localPatterns: [
-      {
-        search: "",
-      },
-    ],
-  },
+	images: {
+		localPatterns: [
+			{
+				search: "",
+			},
+		],
+	},
+	async headers() {
+		return [
+			{
+				source: "/Unity/Build/:path*.gz",
+				headers: [
+					{
+						key: "Content-Encoding",
+						value: "gzip",
+					},
+					{
+						key: "Content-Type",
+						value: "application/javascript",
+					},
+					{
+						key: "Cache-Control",
+						value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
